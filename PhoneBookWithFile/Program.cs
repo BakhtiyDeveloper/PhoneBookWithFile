abdaf1c4d5e565f10c9feff8d5c9966f0e5552a4
BakhtiyDeveloper <bakhtiyer.kurbanov@bk.ru>
2024-07-15 23:16:42 +0500

    Add project files.

---
+++ PhoneBookWithFile/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PhoneBookWithFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine(); 
+        }
+    }
+}
